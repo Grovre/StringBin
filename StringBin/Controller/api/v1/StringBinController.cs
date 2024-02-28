ba3e5b3161b411ef9228367c3c89c7ce8a4abbfb
Grovre <landonvee111@gmail.com>
2024-02-28 11:49:44 -0600

    StringBin init commit

---
+++ StringBin/Controller/api/v1/StringBinController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StringBin.Controller.api.v1;
+
+[Route("/")]
+[ApiController]
+public class StringBinController : ControllerBase
+{
+    [HttpPost("add-entry")]
+    public void AddEntry(string title, string body)
+    {
+        Console.WriteLine($"Title: {title}\nBody:\n{body}");
+    }
+}
---
+++ StringBin/Models/StringBinEntry.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StringBin.Models;
+
+public class StringBinEntry
+{
+    [JsonPropertyName("title")]
+    public string Title { get; }
+    [JsonPropertyName("body")]
+    public string Body { get; }
+
+    public StringBinEntry(string title, string body)
+    {
+        Title = title;
+        Body = body;
+    }
+}
